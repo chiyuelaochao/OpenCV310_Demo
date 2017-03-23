a8be2614b1d8e76e62dbe34fbd098ac6769c08ec
wei.cai <49383207@qq.com>
2017-03-23 15:43:33 +0800

    updated the support file supporting

--- app/src/main/java/com/wongs/facedetectiontest2/MainActivity.java
+++ app/src/main/java/com/wongs/facedetectiontest2/MainActivity.java
@@ -12,6 +12,10 @@ import org.opencv.android.OpenCVLoader;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+
 public class MainActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2 {
     public static final String TAG = "MainActivity";
     private JavaCameraView view;
@@ -34,10 +38,21 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         view = (JavaCameraView) findViewById(R.id.view);
         view.setVisibility(View.VISIBLE);
         view.setCvCameraViewListener(this);
+
+        if (!checkSupportFileExist(FACE_CASCADE_NAME)) {
+            copyResToSdcard("haarcascade_frontalface_default.xml", FACE_CASCADE_NAME);
+        } else {
+            Log.e(TAG, "support file is exist!");
+        }
+
+        if (!checkSupportFileExist(EYES_CASCADE_NAME)) {
+            copyResToSdcard("haarcascade_eye_tree_eyeglasses.xml", EYES_CASCADE_NAME);
+        } else {
+            Log.e(TAG, "support file is exist!");
+        }
     }
 
     @Override
@@ -84,5 +99,42 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
         mRgba = inputFrame.rgba();
         OpencvClass.faceDetection(mRgba.getNativeObjAddr());
         return mRgba;
+    }
+
+    /*
+* 将raw里的文件copy到sd卡下
+* */
+
+    String DATABASE_PATH = "/storage/emulated/0/data/";
+
+    public void copyResToSdcard(String fileName, String outFileName) {//name为sd卡下制定的路径
+        try {
+            File dir = new File(DATABASE_PATH);
+            if (!dir.exists()) {
+                dir.mkdir();
+            }
+            if (!(new File(outFileName)).exists()) {
+//                InputStream is = getResources().openRawResource(rawFileId);
+                InputStream is = getResources().getAssets().open(fileName);
+                FileOutputStream fos = new FileOutputStream(outFileName);
+                byte[] buffer = new byte[1024];
+                int count = 0;
+                while ((count = is.read(buffer)) > 0) {
+                    fos.write(buffer, 0, count);
+                }
+                Log.e(TAG, "copy support files to storage!");
+                fos.close();
+                is.close();
+            }
+        } catch (Exception e) {
+            Log.e(TAG, "Exception : " + e.getMessage());
+        }
+    }
+
+    public static final String FACE_CASCADE_NAME = "/storage/emulated/0/data/haarcascade_frontalface_default.xml";
+    public static final String EYES_CASCADE_NAME = "/storage/emulated/0/data/haarcascade_eye_tree_eyeglasses.xml";
+
+    public boolean checkSupportFileExist(String FilePath) {
+        return new File(FilePath).exists();
     }
 }
